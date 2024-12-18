006b74982aa6baa3376ccb1c954832122472cf4b
ahoshanrafi <md.ahoshanuzzaman5672@gmail.com>
2024-12-18 03:01:21 -0800

    added the template string

---
+++ templatestring.js
@@ -0,0 +1,8 @@
+const numbers=[89,35,98,12];
+const student={
+    name:'salib khan',
+    age:34,     
+    movies:['pushpa','hakar king']
+};
+const about='My name is ${student.name}';
+console.log(about)   
